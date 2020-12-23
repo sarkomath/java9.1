b50d72ed1d6fb69bc2d3ec48132ac29fb916f3f5
Nikita Novoselov <sarkomath@outlook.com>
2020-12-23 21:09:13 +1000

    add fields

--- src/main/package/domain/java/Post.java
+++ src/main/package/domain/java/Post.java
@@ -1,4 +1,7 @@
 public class Post {
+
+    // tage v1
+
     private int id; // уникальный id записи
     private int postMakerId; // указание id автора поста
     private int date; // дата публикации
@@ -8,4 +11,19 @@ public class Post {
     private Likes likes; // количество отметок "Мне нравится" к посту
     private Share share; // количество репостов поста
     private Geografy geografy; // информация о местоположении автора поста
+
+    // tag v2 *no ARRAY
+
+    private int ownerId; // id владельца стены, где расположен пост
+    private int adminId; // id администратора, опубликовавшего запись
+    private int replyOwnerId; // id пользователя, в ответ на запись которого создана запись
+    private int replyPostId; // id записи, на которую опубликован этот пост-ответ
+    private boolean friendsOnly; // видна ли запись только друзьям
+    private String postType; // тип поста
+    private boolean sign; // подписана запись или нет
+    private boolean userCanPin; // может ли пользователь закрепить пост
+    private boolean userCanDel; // может ли пользователь удалить пост
+    private boolean userCanEdit; // может ли пользователь редактировать запись
+    private boolean postPinned; // закреплен ли пост
+    private boolean favorite; // добавлен ли пост в "Избранное"
 }
